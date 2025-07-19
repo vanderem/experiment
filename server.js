06440559552487acff69a4d4951a13974377d3d3
Vander Muniz <67162672+vanderem@users.noreply.github.com>
2025-07-19 17:26:11 -0300

    commit 2

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const path = require('path');
+
+app.use(express.static('experiment'));
+app.use(express.json());
+
+app.post('/data', (req, res) => {
+    const data = JSON.stringify(req.body, null, 2);
+    fs.writeFileSync(path.join(__dirname, 'dados.json'), data);
+    res.sendStatus(200);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
